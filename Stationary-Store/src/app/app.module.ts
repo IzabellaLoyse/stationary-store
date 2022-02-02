4596e45f216292fa7a0d8ada9422c09055a752c6
Izabella Loyse <izabellaloyse13@gmail.com>
2022-02-02 11:40:36 -0300

    feat(product-create): add metodos e funcionalides

--- Stationary-Store/src/app/app.module.ts
+++ Stationary-Store/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
@@ -13,6 +14,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
@@ -36,6 +38,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatListModule,
     MatCardModule,
     MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- Stationary-Store/src/app/components/product/product-create/product-create.component.ts
+++ Stationary-Store/src/app/components/product/product-create/product-create.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
+import { IProduct } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.scss']
+  styleUrls: ['./product-create.component.scss'],
 })
 export class ProductCreateComponent implements OnInit {
+  product: IProduct = {} as IProduct;
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createProduct(): void {
+    this.productService.createProduct(this.product).subscribe(() => {
+      this.productService.showMessage('Produto Criado!');
+      this.router.navigate(['/products']);
+    });
   }
 
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
 }
