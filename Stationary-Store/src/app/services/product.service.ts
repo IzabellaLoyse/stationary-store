bdf970581471689cf168e0272a8acb586514fbcc
Izabella Loyse <izabellaloyse13@gmail.com>
2022-02-02 11:39:49 -0300

    feat(service): Criado o service de Produto

---
+++ Stationary-Store/src/app/services/product.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { IProduct } from '../components/product/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private readonly URL = 'http://localhost:3000/products';
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
+
+  showMessage(message: string) {
+    this.snackBar.open(message, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+
+  createProduct(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>(this.URL, product);
+  }
+}
